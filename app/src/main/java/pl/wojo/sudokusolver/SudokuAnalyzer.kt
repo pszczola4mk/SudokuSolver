c989346cd7fd595686221bf80900630691dbfc7d
pszczola <pszczola@pg.edu.pl>
2021-04-08 10:17:00 +0200

    dodanie wyliczania sudoku

--- app/src/main/java/pl/wojo/sudokusolver/SudokuAnalyzer.kt
+++ app/src/main/java/pl/wojo/sudokusolver/SudokuAnalyzer.kt
@@ -9,6 +9,8 @@ class SudokuAnalyzer() {
 
     var imageBitmap: Bitmap? = null
     var textView: TextView? = null
+    var result: Array<CharArray>? = null
+    var isResult: Boolean = false
 
     constructor(imageBitmap: Bitmap) : this() {
         this.imageBitmap = imageBitmap
@@ -24,6 +26,97 @@ class SudokuAnalyzer() {
             .addOnFailureListener { e ->
                 textView?.text = "BŁĄD"
             }
+    }
+
+    fun solveSudoku(board: Array<CharArray>) {
+        val res = solveSudoku(board, 0, 0)
+        this.isResult = res == 1
+    }
+
+    fun convertedResult(): MutableList<String> {
+        val res: MutableList<String> = mutableListOf()
+        for (i in 0..8) {
+            for (j in 0..8) {
+                res.add(this.result!![i][j].toString())
+            }
+        }
+        return res
+    }
+
+    private fun solveSudoku(board: Array<CharArray>, x: Int, y: Int): Int {
+        var board = board
+        var x = x
+        var y = y
+        return if (board[x][y] == '.') {
+            val allowed = findAllowed(board, x, y) //znajdz dopuszczalne liczby
+            if (allowed.size == 0) {
+                //nic nie znalazł, brak rozwiązania
+                return 0
+            }
+            for (c in allowed) {
+                if (c != '.') {
+                    val copy = copy(board)
+                    copy[x][y] = c
+                    val result = solveSudoku(copy, x, y)
+                    if (result == 1) { //mam rozwiązanie
+                        board = copy
+                        return 1
+                    }
+                }
+            }
+            0
+        } else { //gdy uzupełnione to idź dalej
+            if (x == 8) {
+                if (y == 8) {
+                    this.result = board
+                    return 1 //koniec, przeszedł wszystkie pola
+                }
+                y = y + 1
+            } else {
+                if (y == 8) {
+                    x = x + 1
+                    y = 0
+                } else {
+                    y = y + 1
+                }
+            }
+            solveSudoku(board, x, y)
+        }
+    }
+
+    private fun copy(board: Array<CharArray>): Array<CharArray> {
+        val copy = Array(9) { CharArray(9) }
+        for (i in 0..8) {
+            for (j in 0..8) {
+                copy[i][j] = board[i][j]
+            }
+        }
+        return copy
+    }
+
+    private fun findAllowed(board: Array<CharArray>, x: Int, y: Int): CharArray {
+        val allowed = charArrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9')
+        for (c in board[x]) {
+            clear(allowed, c)
+        }
+        for (c in board.map { it[y] }) {
+            clear(allowed, c)
+        }
+        val xMod = x % 3
+        val yMod = y % 3
+        for (i in x - xMod until x - xMod + 3) {
+            for (j in y - yMod until y - yMod + 3) {
+                val c = board[i][j]
+                clear(allowed, c)
+            }
+        }
+        return allowed
+    }
+
+    private fun clear(allowed: CharArray, c: Char) {
+        if (c != '.') {
+            allowed[(c.toString() + "").toInt() - 1] = '.'
+        }
     }
 
 
--- app/src/main/java/pl/wojo/sudokusolver/SudokuInput.kt
+++ app/src/main/java/pl/wojo/sudokusolver/SudokuInput.kt
@@ -1,14 +1,13 @@
 package pl.wojo.sudokusolver
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TableLayout
-import android.widget.TableRow
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class SudokuInput : AppCompatActivity() {
+    val sudokuVals: MutableList<EditText> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sudoku_input)
@@ -22,9 +21,41 @@ class SudokuInput : AppCompatActivity() {
                 editText.width = 100
                 editText.height = 100
                 editText.id = i * j
-                row.addView(editText,TableRow.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+                row.addView(editText, TableRow.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                sudokuVals.add(editText)
             }
-            sudoku.addView(row, TableLayout.LayoutParams.WRAP_CONTENT,TableLayout.LayoutParams.WRAP_CONTENT)
+            sudoku.addView(row, TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT)
+        }
+
+        val resolveButton = findViewById<Button>(R.id.btn_resolve)
+        resolveButton.setOnClickListener(View.OnClickListener { this.resolve() })
+    }
+
+    private fun resolve() {
+        var analyzer: SudokuAnalyzer = SudokuAnalyzer()
+        val sudoku: Array<CharArray> = Array(9) { CharArray(9) }
+        var row: CharArray = CharArray(9)
+        var counter: Int = 0
+        for (editText: EditText in sudokuVals) {
+            if (counter % 9 == 0) {
+                row = CharArray(9)
+                val idx: Int = counter / 9
+                sudoku[idx] = row
+            }
+            val idx: Int = counter % 9
+            if (editText.text.isNotEmpty()) {
+                row[idx] = editText.text.toList()[0]
+            } else {
+                row[idx] = '.'
+            }
+            counter = counter + 1
+        }
+        analyzer.solveSudoku(sudoku)
+        if (analyzer.isResult) {
+            val solvedSudoku: MutableList<String> = analyzer.convertedResult()
+            for (i in 0..80) {
+                sudokuVals[i].setText(solvedSudoku[i], TextView.BufferType.EDITABLE)
+            }
         }
     }
 }
